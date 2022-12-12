a6b1264491e6cab82cef25b088c5bf0071555f39
urvi141 <109522989+urvi141@users.noreply.github.com>
2022-12-12 13:17:32 +0530

    assignment

---
+++ Python/Assignments/module 3/10.1to30_square.py
@@ -0,0 +1,6 @@
+l = list()
+for i in range(1,30):
+	l.append(i**2)
+print(l)
+print(l[:5])
+print(l[-5:])
---
+++ Python/Assignments/module 3/11.uniquelist.py
@@ -0,0 +1,10 @@
+def get_uniqList(inputList):
+       print('Unique List Is : ',[*set(inputList)]) 
+
+
+a =  [1,2,5,1,8,5,2,6,5,9,26,22,1,1,1,1]
+uniq = get_uniqList(a)
+
+
+
+
