bd11efb7c87e688e2d8e1b8c8c39b6c1c8570f40
urvi141 <109522989+urvi141@users.noreply.github.com>
2022-12-14 17:03:09 +0530

    assignment

---
+++ Python/Assignments/module 3/31.create_dict_from_tuple.py
@@ -0,0 +1,6 @@
+data = {'Bobby': ("chapathi", "sabji"), 
+        'Urvashi': ("Pani puri", "Idly", "Dosa"),
+        'Viram' : ("French fries", "Burger"),
+        'Mihir' : ("Waffers")}
+  
+print(data)
---
+++ Python/Assignments/module 3/43.zip()_function.py
@@ -0,0 +1,6 @@
+Python zip() Function
+
+The zip() function returns a zip object, which is an iterator of tuples where the first item in each passed iterator is paired together, 
+and then the second item in each passed iterator are paired together etc.
+
+Zipped (compressed) files take up less storage space and can be transferred to other computers more quickly than uncompressed files.
